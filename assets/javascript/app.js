98023f62c9f34cf71000707ca9cf0f03076f065e
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-07-22 14:52:54 -0700

    trying to scrape the apis and created gitignore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -61,3 +61,47 @@
           this.src = stillImage;
         }
       })
+      $("button").on("click", function() {
+        // Grabbing and storing the data-animal property value from the button
+        var movies = $(this).attr("data-movie");
+  
+        // Constructing a queryURL using the animal name
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          movies + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+  
+        // Performing an AJAX request with the queryURL
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          // After data comes back from the request
+          .then(function(response) {
+            console.log(queryURL);
+  
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.data;
+  
+            // Looping through each result item
+            for (var i = 0; i < results.length; i++) {
+  
+              // Creating and storing a div tag
+              var movieDiv = $("<div>");
+  
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + results[i].rating);
+  
+              // Creating and storing an image tag
+              var stillImage = $("<img>");
+              // Setting the src attribute of the image to a property pulled off the result item
+              stillImage.attr("src", results[i].images.fixed_height.url);
+  
+              // Appending the paragraph and image tag to the animalDiv
+              movieDiv.append(p);
+              movieDiv.append(stillImage);
+  
+              // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+              $("#gifs-appear-here").prepend(movieDiv);
+            }
+          });
+      });
