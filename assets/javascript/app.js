245b55c7cb958295e2b2c68522cd5a260b26bf89
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-07-23 11:35:41 -0700

    fixed bugs and articulated notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,115 +1,114 @@
 // array of movies
-var movies = ["The Matrix", "The Thing", "Mad Max", "Aliens", "The Good The Bad and The Ugly", "Lord of the Rings", 
-"The Matrix", "Shawn of the Dead", "Kazaam"];
+var movies = ["The Matrix", "The Thing", "Mad Max", "Aliens", "The Good The Bad and The Ugly", "Lord of the Rings",
+  "The Matrix", "Shawn of the Dead", "Kazaam"];
 
 // function for scraping the JSON content for each button into the empty div on the html page
-function displayMovieGif() {
+function showGif() {
   // identifying the created empty div
   $("#movies-view").empty();
   var movie = $(this).attr("data-name");
   // creating a limit of 10 at the end
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  movie + "&api_key=wt7bOsQ6wCxLn0MkwC7AGYQG3xz7BXaN&limit=10";
-  
+    movie + "&api_key=wt7bOsQ6wCxLn0MkwC7AGYQG3xz7BXaN&limit=10";
+
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    
+  }).then(function (response) {
+
     var results = response.data;
-    
+
     for (var i = 0; i < results.length; i++) {
-      
-        // making a div for the scraped gif
-        var movieDiv = $("<div>");
-        movieDiv.attr({"class": "gif"})
-        
-        // Creating an image tag
-        var movieImage = $("<img>");
-        
-        // Giving the image tag an src attribute of a proprty pulled off the
-        // result item
-        movieImage.attr({
-          "src": results[i].images.fixed_height_still.url,
-          "data-still": results[i].images.fixed_height_still.url,
-				  "data-animate": results[i].images.fixed_height.url,
-          "data-state": "still",
-          "class": "gif"
-        });
-        // Appending the paragraph and personImage we created to the "gifDiv" div we created
- 
-        movieDiv.append(movieImage);
-        
-        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-        $("#movies-view").prepend(movieDiv);
-        console.log(movieImage.attr("src"));
-      
+
+      // making a div for the scraped gif
+      var movieDiv = $("<div>");
+      movieDiv.attr({ "class": "gif" })
+
+      // making an image tag
+      var movieImage = $("<img>");
+
+      // giving the image tag an src attribute of a proprty pulled off the result item from the site itself
+      movieImage.attr({
+        "src": results[i].images.fixed_height_still.url,
+        "data-still": results[i].images.fixed_height_still.url,
+        "data-animate": results[i].images.fixed_height.url,
+        "data-state": "still",
+        "class": "gif"
+      });
+      // appending the movieImage we created to the movieDiv div we created
+
+      movieDiv.append(movieImage);
+
+      // prepending the movieDiv to the "#movies-view" div in the HTML
+      $("#movies-view").prepend(movieDiv);
+      console.log(movieImage.attr("src"));
+
     }
-    $(".gif").on("click", function() {
-			
-			// $(this) just means "the element with class 'gif' that was clicked" from shoe!
-		   var state = $(this).attr("data-state");
-		   
-		   // $(this).attr("data-state") will either be "still" or "animate" from shoe and class 6.2
-		   // IF it's still: we change it to animate
-		   if (state === "still") {
-			   
-			   var newSrc = $(this).attr("data-animate");
-			   $(this).attr("src", newSrc);
-			   $(this).attr("data-state", "animate");
-			   
-			// OTHERWISE it's animated already, so we change it to still so its an easy back and forth
-		   } else {
-			   var newSrc = $(this).attr("data-still");
-			   $(this).attr("src", newSrc);
-			   $(this).attr("data-state", "still");
-		   }
-	   }); 
+    $(".gif").on("click", function () {
+
+      // $(this) just means "the element with class 'gif' that was clicked" from shoe!
+      var state = $(this).attr("data-state");
+
+      // $(this).attr("data-state") will either be "still" or "animate" from shoe and class 6.2
+      // IF it's still: we change it to animate
+      if (state === "still") {
+
+        var newSrc = $(this).attr("data-animate");
+        $(this).attr("src", newSrc);
+        $(this).attr("data-state", "animate");
+
+        // OTHERWISE it's animated already, so we change it to still so its an easy back and forth
+      } else {
+        var newSrc = $(this).attr("data-still");
+        $(this).attr("src", newSrc);
+        $(this).attr("data-state", "still");
+      }
+    });
   });
 };
 
-// Function for displaying movie data
+// function for displaying movie data
 function renderButtons() {
-  
-  // Deleting the buttons prior to adding new movies
+
+  // deleting the buttons prior to adding new movies
   // (this is necessary otherwise you will have repeat buttons)
   $("#buttons-view").empty();
-  
-  // Looping through the array of movies
+
+  // looping through the array of movies so that all 10 will populate
   for (var i = 0; i < movies.length; i++) {
-    
-    // Then dynamically generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+
+    // then dynamically generating buttons for each movie in the array
+    // this code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>) but im still putting it on the buttons view tag in html
     var a = $("<button>");
-    // Adding a class of movie to our button
+    // adding a class of movie to our button
     a.addClass("movie");
-    // Adding a data-attribute
+    // adding a data-attribute running through the loop
     a.attr("data-name", movies[i]);
-    // Providing the initial button text
+    // providing the initial button text which runs through the loop
     a.text(movies[i]);
-    // Adding the button to the buttons-view div
+    // adding the button to the buttons-view div
     $("#buttons-view").append(a);
   }
 }
 
-// This function handles events where one button is clicked
-$("#add-movie").on("click", function(event) {
+// this function handles events where one button is clicked to put our new movie
+$("#add-movie").on("click", function (event) {
   event.preventDefault();
 
-  // This line grabs the input from the textbox
+  // this line grabs the input from the textbox
   var movie = $("#movie-input").val().trim();
 
-  // Adding the movie from the textbox to our array
+  // adding the movie from the textbox to our array
   movies.push(movie);
   console.log(movies);
 
-  // Calling renderButtons which handles the processing of our movie array
+  // calling renderButtons which makes the new button
   renderButtons();
 });
 
-// Function for displaying the movie info
-// Using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
-$(document).on("click", ".movie", displayMovieGif);
+// function for displaying the movie info
+// using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements on the whole page
+$(document).on("click", ".movie", showGif);
 
-// Calling the renderButtons function to display the initial buttons
+// using the renderButtons function to make the initial buttons we established
 renderButtons();
